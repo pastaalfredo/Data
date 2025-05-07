c2560a16acffa61e2694578b7eddd71561df48a9
David van der Spoel <david.vanderspoel@gmail.com>
2025-05-07 19:54:12 +0200

    Updated script for ESP RMSD

--- Electrostatics-ESP-ACT/ESPcheck/calc_esp.py
+++ Electrostatics-ESP-ACT/ESPcheck/calc_esp.py
@@ -2,10 +2,13 @@
 
 import os
 
-def run_one(ff:str)->float:
+def run_one(ff:str, qtype:str)->float:
     myff = os.path.basename(ff)
     mylog = myff.replace(".xml", ".log")
-    os.system("alexandria train_ff -ff ../AlexandriaFF/%s -mp ../../../ACTdata/MolProps/hf-aug-cc-pvtz.xml -nooptimize -sel ../Selection/monomer.dat -g %s -v 4" % ( myff, mylog ) )
+    cmd = ("alexandria train_ff -ff ../AlexandriaFF/%s -mp ../../../ACTdata/MolProps/hf-aug-cc-pvtz.xml -nooptimize -sel ../Selection/monomer.dat -g %s -v 4" % ( myff, mylog ) )
+    if qtype:
+        cmd += (" -qtype q%s -charges ../AlexandriaFF/esp-paper-gaussian.xml " % qtype)
+    os.system(cmd)
     rmsd = None
     with open(mylog, "r") as inf:
         for line in inf:
@@ -18,17 +21,68 @@ def run_one(ff:str)->float:
     return rmsd
     
 if __name__ == "__main__":
-    allffs = [ "all-p.xml", "coul-gv.xml", "esp-gv.xml", "all-g.xml",
-    	       "all-pg.xml", "coul-p.xml", 
-               "all-gv.xml", "coul-g.xml", "esp-g.xml" ]
-    myrmsd = {}
+    allffs = [
+        "Mulliken",
+        "Hirshfeld",
+        "ESP",
+        "CM5",
+        "BCC",
+        "RESP",
+        "esp-g.xml",
+        "esp-gv.xml",
+        "coul-p.xml", 
+        "all-p.xml", 
+        "coul-g.xml",
+        "all-g.xml",
+        "coul-gv.xml",
+        "all-gv.xml",
+    	"all-pg.xml",
+    ]
+    label = {
+        "esp-g.xml": { "model": "GC", "train": "ESP" },
+        "esp-gv.xml": { "model": "GC+PGV", "train": "ESP" },
+        "coul-p.xml": { "model": "PC", "train": "SAPT (Elec)" },
+        "all-p.xml": { "model": "PC", "train": "SAPT (Elec+Induc)" },
+        "coul-g.xml": { "model": "GC", "train": "SAPT (Elec)" },
+        "all-g.xml": { "model": "GC", "train": "SAPT (Elec+Induc)" },
+        "coul-gv.xml": { "model": "GC+PGV", "train": "SAPT (Elec)" },
+        "all-gv.xml": { "model": "GC+PGV", "train": "SAPT (Elec+Induc)" },
+    	"all-pg.xml": { "model": "PC+GVS", "train": "SAPT (Elec,Induc)" }
+    }
+    myrmsd = []
     for ff in allffs:
-        print("Will run %s" % ff)
-        rmsd = run_one(ff)
+        myff  = "coul-p.xml"
+        qtype = ff
+        myid  = qtype
+        if ff.endswith(".xml"):
+            myff  = ff
+            qtype = None
+            myid  = myff
+        print("Will run %s" % myff)
+        rmsd = run_one(myff, qtype)
         print("==================")
         if rmsd:
-            myrmsd[ff[:-4]] = rmsd
-    for key in sorted(myrmsd.keys()):
-        val = myrmsd[key]
-        print("%s  %g" % ( key, val ))
-
+            myrmsd.append( ( myid,  rmsd ) )
+    for kv in myrmsd:
+        print("%s  %g" % ( kv[0], kv[1] ))
+    tab = "esprmsd.tex"
+    with open(tab, "w") as outf:
+        outf.write("\\begin{table}[ht]\n")
+        outf.write("\\centering\n")
+        outf.write("\\caption{RMSD (kJ/mol e) of different charge models with respect to the electrostatic potential computed at the HF/aug-cc-pvtz level of theory (see Methods). For description of the different models and training, see Methods.}\n")
+        outf.write("\\label{tab:esprms}\n")
+        outf.write("\\begin{tabular}{lcc}\n")
+        outf.write("\\hline\n")
+        outf.write("Model & Training target & RMSD \\\\\n")
+        outf.write("\\hline\n")
+        for kv in myrmsd:
+            model = kv[0]
+            train = ""
+            if kv[0] in label:
+                model = label[kv[0]]["model"]
+                train = label[kv[0]]["train"]
+            outf.write("%s & %s & %.1f\\\\\n" % ( model, train, kv[1] ))
+        outf.write("\\hline\n")
+        outf.write("\\end{tabular}\n")
+        outf.write("\\end{table}\n")
+    print("Please check %s" % tab)
